977142c7ca146e8e3efc0a4c444ea07acd0f1502
nsonneborn <nsonneborn@middlebury.edu>
2017-10-19 00:26:35 +0200

    modal adjustments, labels recoded

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+// just tryna play with regular expresions
+function validatePassword() {
+  var psw = blah // fill this in lol
+  if (!/^\w{1,8}$/.test(psw)){
+    alert("Password can only contain number and letter characters and has maximum length 8")
+  }
+}
+
+
 function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
@@ -28,5 +38,42 @@ var modal = document.getElementById('id01');
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+
+
+// Cookies stuff skeleton from W3schools
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
     }
 }
