98a1a3c5a6d9f8095cb33fb410d39c0e9bef7d7d
nsonneborn <nsonneborn@middlebury.edu>
2017-10-25 14:27:52 +0200

    cookies that kinda work

--- script.js
+++ script.js
@@ -3,86 +3,100 @@
 function validatePassword() {
   var psw = blah // fill this in lol
   if (!/^\w{1,8}$/.test(psw)){
-    alert("Password can only contain number and letter characters and has maximum length 8")
+    alert("Password can only contain number and letter characters and has maximum length 8");
   }
 }
 function validateForm(){
-  validateEmail()
-
+  validateEmail();
 }
 function validateEmail()
 {
  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(personalinfo.email.value))
   {
-    return (true)
+    return (true);
   }
     alert("You have entered an invalid email address!")
-    return (false)
+    return (false);
 }
 
 
 // Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 
 // Cookies stuff skeleton from W3schools
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue;
+    console.log("set cookie called");
 }
 
- function saveCookie(){
-   var username = document.modal.email.value;
-   var password = document.modal.psw.value;
-   var fname = document.personalinfo.fname.value;
-   var lname = document.personalinfo.lname.value;
-   var email = document.personalinfo.email.value;
-   var dob = document.personalinfo.dob.value;
-   var lang = document.personalinfo.lang.value;
-   var profpic = document.personalinfo.profpic.value;
-   document.cookie = "user=" + username + ";" + "password=" + password + ";" +
-    "firstName=" + fname +";" + "lastName=" +
-   lname +";"+ "email=" + email +";" + "dateOfBirth" + dob +";"
-   + "language=" + lang +";" + "profPic" + profic+";"
-
- }
+function modalSubmit(){
+  var email = $('#emailModal').val();
+  var password = $('#passwordModal').val();
+  checkCookie(email, password);
+  console.log(document.cookie);
+  document.getElementById('id01').style.display='none';
+}
 
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  var name = cname + "=";
+  var cookieArray = document.cookie.split(';');
+  for(var i = 0; i < cookieArray.length; i++) { // for every cookie in the array
+    var c = cookieArray[i]; // get the substring
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    return "";
+  return " ";
 }
 
-function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
+function checkCookie(email, password) {
+    if (email == getCookie('email') && password == getCookie('password')) {
+        console.log("recognized email and password");
+        fillInfo();
     } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
+      delete_cookie(document.cookie);
+      setCookie('email', email);
+      setCookie('password', password);
+      console.log("didn't recognize email/pass");
         }
-    }
 }
+
+function fillInfo(){
+  $('#fname').val(getCookie('fname'));
+  $('#lname').val(getCookie("lname"));
+  $('#email').val(getCookie("email"));
+  $('#dob').val(getCookie("dob"));
+  $('#lang').val(getCookie("lang"));
+  $('#profpic').val(getCookie("profpic"));
+}
+
+function saveCookie(){
+  var username = document.modal.email.value;
+  var password = document.modal.password.value;
+  var fname = document.personalinfo.fname.value;
+  var lname = document.personalinfo.lname.value;
+  var email = document.personalinfo.email.value;
+  var dob = document.personalinfo.dob.value;
+  var lang = document.personalinfo.lang.value;
+  var profpic = document.personalinfo.profpic.value;
+  setCookie("username", username);
+  setCookie("password", password);
+  setCookie("fname", fname);
+  setCookie("lname", lname);
+  setCookie("email", email);
+  setCookie("dob", dob);
+  setCookie("lang", lang);
+  setCookie("profpic", profpic);
+}
+
+function delete_cookie(name){
+   document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
 
 function optionCheck() {
   selectOptions = document.getElementById("paymentmethod");
